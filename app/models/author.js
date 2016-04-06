8e83b3fd9728ff747e5c93679bdc43211435679e
Malik Dixon <mdixon@rap.midco.net>
2016-04-05 22:01:48 -0600

    updated files

---
+++ app/models/author.js
@@ -0,0 +1,16 @@
+// app/models/author.js
+import DS from 'ember-data';
+import Faker from 'faker';
+
+export default DS.Model.extend({
+
+  name: DS.attr('string'),
+
+  books: DS.hasMany('book', {inverse: 'author'}),
+
+  randomize() {
+    this.set('name', Faker.name.findName());
+    return this;
+  }
+
+});
